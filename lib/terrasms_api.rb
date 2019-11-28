a576655c0e08e397dc62df1412b95bf670e22824
wowinter13 <vla-dy@yandex.ru>
2019-11-28 15:23:00 +0300

    Initialize Gem structure

---
+++ lib/terrasms_api.rb
@@ -0,0 +1,6 @@
+require "terrasms_api/version"
+
+module TerrasmsApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/terrasms_api/version.rb
@@ -0,0 +1,3 @@
+module TerrasmsApi
+  VERSION = "0.1.0"
+end
